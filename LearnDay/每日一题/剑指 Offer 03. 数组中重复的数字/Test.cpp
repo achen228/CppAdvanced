3efa1d89c25850abbeef499826b39e02773619aa
achen228 <achen228@163.com>
2020-09-15 15:26:17 +0800

    Update Test.cpp

--- LearnDay/每日一题/剑指 Offer 03. 数组中重复的数字/Test.cpp
+++ LearnDay/每日一题/剑指 Offer 03. 数组中重复的数字/Test.cpp
@@ -1,3 +1,4 @@
+//使用set中的count判断
 class Solution {
 public:
     int findRepeatNumber(vector<int>& nums) {
@@ -11,6 +12,23 @@ public:
             else
             {
                 s.insert(e);
+            }
+        }
+        return -1;
+    }
+};
+
+
+//先排序，后查找
+class Solution {
+public:
+    int findRepeatNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for(int i = 1; i < nums.size(); i++)
+        {
+            if(nums[i] == nums[i - 1])
+            {
+                return nums[i];
             }
         }
         return -1;
