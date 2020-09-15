31a4af8222b53ad2ed1e4d9daec186a7c0274888
achen228 <achen228@163.com>
2020-09-15 18:41:45 +0800

    Update Test.cpp

--- LearnDay/每日一题/剑指 Offer 04. 二维数组中的查找/Test.cpp
+++ LearnDay/每日一题/剑指 Offer 04. 二维数组中的查找/Test.cpp
@@ -21,3 +21,38 @@ public:
         return false;
     }
 };
+
+
+//时间复杂度O(m + n)
+class Solution {
+public:
+    bool findNumberIn2DArray(vector<vector<int>>& matrix, int target) {
+        if(matrix.empty())
+        {
+            return false;
+        }
+
+        int rowsize = matrix.size(), colsize = matrix[0].size();
+        int row = 0, col = colsize - 1;
+        while(row < rowsize && col >= 0)
+        {
+            //从矩阵的右上角开始匹配，如果等于target，则返回true
+            if(matrix[row][col] == target)
+            {
+                return true;
+            }
+            //如果小于target，说明这一行都比target小，row向下移动一行
+            else if(matrix[row][col] < target)
+            {
+                row++;
+            }
+            //如果大于target，说明这一列都比target大，col向左移动一列
+            else
+            {
+                col--;
+            }
+        }
+
+        return false;
+    }
+};
