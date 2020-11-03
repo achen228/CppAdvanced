43136e49d239b76bbb4ddbb577171abc6cf5ad98
achen <achen228@163.com>
2020-11-03 22:04:28 +0800

    add code

--- LearnDay/每日一题/剑指 Offer 27. 二叉树的镜像/Test.cpp
+++ LearnDay/每日一题/剑指 Offer 27. 二叉树的镜像/Test.cpp
@@ -1,1 +1,30 @@
+/**
+* Definition for a binary tree node.
+* struct TreeNode
+* {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+**/
+class Solution {
+  public:
+    void SwapNode(TreeNode* node)
+    {
+      if(!node || (!node->left && !node->right))
+        return;
+      TreeNode* tmp = node->left;
+      node->left = node->right;
+      node->right = tmp;
+    }
 
+    TreeNode* mirrorTree(TreeNode* root) {
+      if(!root || (!root->left && !root->right))
+        return root;
+      SwapNode(root);
+      mirrorTree(root->left);
+      mirrorTree(root->right);
+      return root;
+    }
+};
